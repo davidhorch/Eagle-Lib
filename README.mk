6f1bebbfafea7e53d2caee2866fb37c25081d0a3
David Horch <david.horch@uni-bremen.de>
2018-07-07 06:51:58 +0200

    Added a readme file

---
+++ README.mk
@@ -0,0 +1,12 @@
+English:
+A collection of (mostly) selfmade eagle library files for several electrical parts needed in own projects.
+Usually I try to reuse already existing packages if possible, otherwise I create my own.
+
+All library files are free to use on your own risk.
+All parts here have been tested on at least one PCB.
+
+German:
+Eine Sammlung meiner eigenen Bauteile für Eagle. Üblicherweise nutze ich bereits existierende Footprints, es sei denn, es geht nicht anders.
+
+Alle hier aufgelisteten Bibliotheken können frei verwendet werden.
+Ich übernehme keine Haftung für fehlerhafte Bauteile, auch wenn nur solche veröffentlicht werden, die mindestens einmal auf eigenen Leiterkarten getestet wurden.
