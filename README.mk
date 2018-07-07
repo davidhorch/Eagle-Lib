e36b6908033c2a29076d2fb2807db73f4e282c5c
David Horch <david.horch@uni-bremen.de>
2018-07-07 07:09:21 +0200

    Updated the readme file

--- README.mk
+++ README.mk
@@ -5,8 +5,19 @@ Usually I try to reuse already existing packages if possible, otherwise I create
 All library files are free to use on your own risk.
 All parts here have been tested on at least one PCB.
 
+Naming pattern is simply <manufacturer's part number (MPN)>.lbr
+You can find (at least) two attributes in the files. One is the MPN and the other one (MFG) contains the manufacturer of the part.
+
 German:
 Eine Sammlung meiner eigenen Bauteile für Eagle. Üblicherweise nutze ich bereits existierende Footprints, es sei denn, es geht nicht anders.
 
 Alle hier aufgelisteten Bibliotheken können frei verwendet werden.
 Ich übernehme keine Haftung für fehlerhafte Bauteile, auch wenn nur solche veröffentlicht werden, die mindestens einmal auf eigenen Leiterkarten getestet wurden.
+
+Die Bibliothek wird ständig erweitert.
+
+Namensschema:
+<Herstellerbezeichnung>.lbr
+
+Es existieren zwei Attribute, die den Herstellercode (MPN) sowie den Hersteller (MFG) selbst kennzeichnen.
+Das mag einigen Standardbauteilen nicht gerecht werden (Es gibt sie von mehreren Herstellern), macht aber bei etwas speziellen Sachen und damit generell Sinn.
